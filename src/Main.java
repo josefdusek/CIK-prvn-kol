7afea5a74d2817a3cf3b31235c86a8d906bbd4ba
josefdusek <144115929+josefdusek@users.noreply.github.com>
2025-09-21 17:02:49 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        int[][] Matrix1 = {{1,2,3},{0,1,4},{5,6,0}};
-        int[][] Matrix2 = {{7,8,9,10},{2,3,4,5},{1,0,-1,2}};
+        int[][] Matrix1 = {
+                {4, 3, 2},
+                {1, 2, 7},
+                {3, 2, 4}
+        };
+        int[][] Matrix2 = {
+                {2, 1, 3, 0},
+                {9, 10, 3, 2},
+                {0, 3, 8, 4}
+        };
 
-        Matrix matr = new Matrix(); matr.setMatrix(Matrix1);
-        Matrix matr2 = new Matrix(); matr2.setMatrix(Matrix2);
+        MatrixReal matrix = new MatrixReal();
+        matrix.setMatrix(Matrix1);
+        MatrixReal matrix2 = new MatrixReal();
+        matrix2.setMatrix(Matrix2);
 
-        IMatrix sum = matr.add(matr2);
-        IMatrix a = matr.times(matr2);
-        IMatrix b = matr.transpose();
-
-        Matrix.printMatrix(matr);
-        Matrix.printMatrix(matr2);
-        Matrix.printMatrix(sum);
-        Matrix.printMatrix(a);
-        Matrix.printMatrix(b);
-
-        System.out.println("ctvercova: " + matr.isSquare());
+        
+        System.out.println("Matrix times: ");
+        MatrixReal.print(matrix.times(matrix2));
+        
+        System.out.println("Matrix times Scalar: ");
+        MatrixReal.print(matrix.times(5));
+        
+        System.out.println("Matrix add:");
+        MatrixReal.print(matrix.add(matrix2));
+        
+        System.out.println("Matrix add:");
+        MatrixReal.print(matrix.add(matrix2));
+        
+        System.out.println("Matrix transpose:");
+        MatrixReal.print(matrix.transpose());
+        
+        System.out.println("ctvercova: " + matrix.isSquare());
+        System.out.println("Get Trace: " + matrix.getTrace());
     }
 
-}
